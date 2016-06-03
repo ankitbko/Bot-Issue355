f06c5cdb9843d9bc60a4e57ddf4be9d4e407fd0b
'Ankit <ankitbko@gmail.com>
2016-06-03 09:41:43 +0530

    Updated BotBuilder Framework to 1.2.3

--- BotIssue/CityLuisDialog.cs
+++ BotIssue/CityLuisDialog.cs
@@ -67,10 +67,11 @@ namespace BotIssue
     public class CityForm
     {
         public string Location { get; set; }
+        public string Name { get; set; }
 
         public static IForm<CityForm> BuildForm()
         {
-            string Airports = "Aberdeen; Aberdeen Airport Belfast; Belfast International Airport Aldergrove Belfast; George Best Belfast City Airport Birmingham; Birmingham International Airport Blackpool; Blackpool Airport Bristol; Bristol Airport Cardiff; Cardiff International Airport Derby; East Midlands Apt Doncaster; Robin Hood Airport Durham; Durham Tees Valley Airport Edinburgh; Edinburgh Airport Exeter; Exeter Airport Glasgow; Glasgow International Airport Gloucester; Staverton Airport Humberside; Humberside Airport Inverness; Inverness Airport Jersey; Jersey Airport (Meet&Greet) Leeds Bradford Airport; (Meet & Greet) Liverpool; Liverpool Airport London; Gatwick Airport North Terminal London; Gatwick Airport South Terminal London; London Heathrow Airport, T1,T2,T3,T4 London; London Heathrow Airport, T5 London; London City Airport London; London Luton Airport London; London Stansted Airport Manchester; Manchester Airport Newcastle; Newcastle Airport Newquay; Newquay Airport Norwich; Norwich Airport Prestwick; Prestwick Airport Southampton; Southampton International Airport";
+            string Airports = "Aberdeen; Aberdeen Airport Belfast; Belfast International Airport Aldergrove Belfast; George Best Belfast City Airport Birmingham; Birmingham International Airport Blackpool; Blackpool Airport Bristol; Bristol Airport Cardiff; Cardiff International Airport Derby; East Midlands Apt Doncaster; Robin Hood Airport Durham; Durham Tees Valley Airport Edinburgh; Edinburgh Airport Exeter; Exeter Airport Glasgow; Glasgow International Airport Gloucester; Staverton Airport Humberside; Humberside Airport Inverness; Inverness Airport Jersey; Jersey Airport Meet&Greet Leeds Bradford Airport; Meet & Greet Liverpool; Liverpool Airport London; Gatwick Airport North Terminal London; Gatwick Airport South Terminal London; London Heathrow Airport, T1,T2,T3,T4 London; London Heathrow Airport, T5 London; London City Airport London; London Luton Airport London; London Stansted Airport Manchester; Manchester Airport Newcastle; Newcastle Airport Newquay; Newquay Airport Norwich; Norwich Airport Prestwick; Prestwick Airport Southampton; Southampton International Airport";
             //This Causes issue
             //(Meet & Greet) Liverpool
             //string Airports = "Liverpool Airport London; (Meet & Greet) Liverpool; Gatwick Airport North Terminal London"; //WTF
@@ -82,7 +83,7 @@ namespace BotIssue
                         foreach (var airport in Airports.Split(';'))
                         {
                             field.AddDescription(airport, Language.CamelCase(airport));
-                            field.AddTerms(airport, Language.GenerateTerms(Language.CamelCase(airport), 3));  // change to 5
+                            field.AddTerms(airport, Language.GenerateTerms(Language.CamelCase(airport), 5));  // change to 5
                         }
                         return true;
                     })
